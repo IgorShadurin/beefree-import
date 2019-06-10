f319bdcffa46d7525c1a5a5464261be4ffb179c3
shadurin <igor.shadurin@gmail.com>
2019-06-10 12:18:23 +0300

    added data using visualization for dropbox

--- src/Layout.js
+++ src/Layout.js
@@ -12,7 +12,15 @@ class Layout extends Component {
             instagramError: ''
         };
         this.instagramModal = null;
+        this.onDropboxReceiveInfo();
     }
+
+    formatBytes = (a, b) => {
+        if (0 == a) return "0 Bytes";
+        var c = 1024, d = b || 2, e = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"],
+            f = Math.floor(Math.log(a) / Math.log(c));
+        return parseFloat((a / Math.pow(c, f)).toFixed(d)) + " " + e[f]
+    };
 
     getDataStata = () => {
         let dataStata = {};
@@ -94,6 +102,132 @@ class Layout extends Component {
             });
     };
 
+    onDropboxReceiveInfo() {
+        if (window.location.hash.indexOf('access_token') === -1) {
+            return;
+        }
+
+        //todo display wait animation
+        const self = this;
+
+        function getHashValue(key) {
+            var matches = window.location.hash.match(new RegExp(key + '=([^&]*)'));
+            return matches ? matches[1] : null;
+        }
+
+        var statshow, statshowa, statshowb, statshowc;
+
+
+        var uid = getHashValue('uid');
+        var account_id = getHashValue('account_id');
+        var access_token = getHashValue('access_token');
+        var token_type = getHashValue('token_type');
+
+        // alert(access_token);
+
+
+        window.jQuery.ajax({
+            url: 'https://api.dropboxapi.com/2/users/get_account',
+            method: "POST",
+            dataType: "json",
+            crossDomain: true,
+            contentType: "application/json; charset=utf-8",
+            data: '{"account_id":"' + decodeURIComponent(account_id) + '"}',
+            cache: false,
+            beforeSend: function (xhr) {
+                /* Authorization header */
+                xhr.setRequestHeader("Authorization", 'Bearer ' + access_token);
+            },
+            success: function (data) {
+                console.log(data);
+
+                statshowa = 'Name: <strong> ' + data.name.display_name + '</strong> with email <strong>' + data.email + '</strong>';
+                //window.$('#root').html(statshowa);
+                self.setDataStata('dropbox', 'display_name', data.name.display_name);
+                self.setDataStata('dropbox', 'email', data.email);
+
+                window.$.ajax({
+                    url: 'https://api.dropboxapi.com/2/users/get_space_usage',
+                    method: "POST",
+                    cache: false,
+                    beforeSend: function (xhr) {
+                        xhr.setRequestHeader("Authorization", 'Bearer ' + access_token);
+                    },
+                    success: function (data) {
+                        console.log(data);
+                        statshowb = '<p>Total <strong>' + self.formatBytes(data.used, 2) + '</strong> data stored of <strong>' + self.formatBytes(data.allocation.allocated, 2) + '</strong></p>';
+                        self.setDataStata('dropbox', 'data_used', data.used);
+                        self.setDataStata('dropbox', 'data_allocated', data.allocation.allocated);
+
+                        //window.$('#root').append(statshowb);
+
+                        window.$.ajax({
+                            url: 'https://api.dropboxapi.com/2/files/list_folder',
+                            method: "POST",
+                            dataType: "json",
+                            crossDomain: true,
+                            contentType: "application/json; charset=utf-8",
+                            data: '{"path": "","recursive": false,    "include_media_info": false,    "include_deleted": false,   "include_has_explicit_shared_members": false,    "include_mounted_folders": true,    "include_non_downloadable_files": true}',
+                            cache: false,
+                            beforeSend: function (xhr) {
+                                xhr.setRequestHeader("Authorization", 'Bearer ' + access_token);
+                            },
+                            success: function (data) {
+                                console.log(data);
+                                var filelist = '';
+                                window.$.each(data.entries, function (key, value) {
+
+                                    //alert(JSON.stringify(value[".tag"]));
+                                    if (value[".tag"] == 'folder') filelist = filelist + '<i class="fas fa-folder"></i> ';
+                                    if (value[".tag"] == 'file') filelist = filelist + '<i class="fas fa-file"></i> ';
+                                    filelist = filelist + value["name"] + '  ';
+                                    if (value[".tag"] == 'file') {
+                                        filelist = filelist + '<i>' + self.formatBytes(value["size"], 2) + '</i>  ';
+                                        // filelist=filelist+value["client_modified"]+'  ';
+                                    }
+
+                                    filelist = filelist + '<br/>';
+                                });
+
+                                statshowc = filelist;
+                                /*window.$('#root').append(filelist);
+                                window.parent.$("#dropbox-data").html(statshowa + statshowb + statshowc);
+                                if (window.opener && !window.opener.closed)
+                                    window.opener.$("#dropbox-data").html(statshowa + statshowb + statshowc);
+
+
+                                window.parent.$("#view-dropbox-btn").hide();
+                                if (window.opener && !window.opener.closed)
+                                    window.opener.$("#view-dropbox-btn").hide();
+
+                                window.parent.$("#import-dropbox-btn").show();
+                                if (window.opener && !window.opener.closed)
+                                    window.opener.$("#import-dropbox-btn").show();
+
+                                //self.close();
+
+*/
+                            },
+                            error: function (jqXHR, textStatus, errorThrown) {
+                                alert(JSON.stringify(jqXHR) + 'Error:' + textStatus + 'Thrown:' + errorThrown);
+                            }
+                        });
+
+
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        //  alert (JSON.stringify(jqXHR)+'Error:'+textStatus+'Thrown:'+errorThrown);
+                    }
+                });
+
+
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert(JSON.stringify(jqXHR) + 'Error:' + textStatus + 'Thrown:' + errorThrown);
+            }
+        });
+    }
+
     connectInstagram = () => {
         this.setState({
             instagramUsername: ''
@@ -102,6 +236,7 @@ class Layout extends Component {
     };
 
     connectDropbox = () => {
+        window.open(`https://www.dropbox.com/oauth2/authorize?client_id=ntd2thawpc8o1sq&response_type=token&redirect_uri=${document.URL}`, '_self', 'width=800, height=600');
     };
 
     connectGoogleDrive = () => {
@@ -116,6 +251,11 @@ class Layout extends Component {
     connectTwitter = () => {
     };
 
+    forgetAll = () => {
+        localStorage.setItem('data_stata', '');
+        this.setState({dataStata: {}});
+    };
+
     notImplemented = (e) => {
         e.preventDefault();
         alert('Not implemented');
@@ -125,7 +265,22 @@ class Layout extends Component {
         const {dataStata, instagramProcess, instagramError} = this.state;
 
         let instagramData = {};
+        let dropboxData = {};
         const instagramOptions = {
+            responsive: true,
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: '...'
+            },
+            animation: {
+                animateScale: true,
+                animateRotate: true
+            }
+        };
+        const dropboxOptions = {
             responsive: true,
             legend: {
                 position: 'top',
@@ -154,8 +309,29 @@ class Layout extends Component {
                     label: 'Dataset 1'
                 }],
                 labels: [
-                    'Photos',
-                    'Videos'
+                    `Photos (${dataStata.instagram.photos})`,
+                    `Videos (${dataStata.instagram.videos})`
+                ]
+            };
+        }
+
+        if ("dropbox" in dataStata) {
+            dropboxOptions.title.text = 'Email: ' + dataStata.dropbox.email;
+            dropboxData = {
+                datasets: [{
+                    data: [
+                        dataStata.dropbox.data_used,
+                        dataStata.dropbox.data_allocated - dataStata.dropbox.data_used
+                    ],
+                    backgroundColor: [
+                        'rgb(75, 192, 192)',
+                        'rgb(54, 162, 235)',
+                    ],
+                    label: 'Dataset 1'
+                }],
+                labels: [
+                    `Used ${this.formatBytes(dataStata.dropbox.data_used, 2)}`,
+                    `Free space ${this.formatBytes(dataStata.dropbox.data_allocated - dataStata.dropbox.data_used, 2)}`
                 ]
             };
         }
@@ -223,6 +399,8 @@ class Layout extends Component {
                     <a className="btn btn-outline-primary" href="signin.html" onClick={this.notImplemented}>Sign
                         in</a> &nbsp;
                     <a className="btn btn-outline-primary" href="signup.html" onClick={this.notImplemented}>Sign up</a>
+                    &nbsp;
+                    <button className="btn btn-outline-primary" onClick={this.forgetAll}>Forget all</button>
                 </div>
 
                 <div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
@@ -260,19 +438,6 @@ class Layout extends Component {
                                             data-target="#instagramModal">
                                         View Data
                                     </button>
-
-                                    {/*<a className="btn btn-primary" id="import-instagram-btn"
-                                       href="import_instagram_files.html"
-                                       style={{display: 'none'}}>
-                                        <span style={{color: '#ffffff !important'}}>Import full data of Instagram</span></a>*/}
-
-
-                                    {/*<div className="embed-responsive embed-responsive-16by9" style={{display: 'none'}}>
-                                        <iframe className="embed-responsive-item"
-                                                src="https://api.instagram.com/oauth/authorize/?client_id=4d13c46e2674470b8ca898b7aa7954b3&redirect_uri=https://c3web.io/beefree/import_instagram.html&response_type=token">
-                                            Loading ...
-                                        </iframe>
-                                    </div>*/}
                                 </div>
                             </div>
 
@@ -283,31 +448,19 @@ class Layout extends Component {
                                 <div className="card-header"><strong>Dropbox</strong></div>
                                 <div className="card-body">
                                     <h5 className="card-title"></h5>
-                                    <p className="card-text" id="dropbox-data">Dropbox is a file hosting service
-                                        operated
-                                        by the company
-                                        Dropbox, Inc., headquartered in San Francisco, California.</p>
+                                    <p className="card-text" id="dropbox-data">
+                                        {!("dropbox" in dataStata) &&
+                                        <span>Dropbox is a file hosting service operated by the company Dropbox, Inc., headquartered in San Francisco, California.</span>}
+
+                                        {"dropbox" in dataStata &&
+                                        <Doughnut data={dropboxData} width={400} height={400}
+                                                  options={dropboxOptions}/>}
+                                    </p>
                                     <button className="btn btn-primary" id="view-dropbox-btn"
-                                        //onClick="window.open('https://www.dropbox.com/oauth2/authorize?client_id=ycvhx8dgtp4y9ql&response_type=token&redirect_uri=https://c3web.io/beefree/import_dropbox.html','InstagramWindow', 'width=800, height=600');"
                                             onClick={this.connectDropbox}
                                     >
                                         View data
                                     </button>
-
-                                    <a className="btn btn-primary" id="import-dropbox-btn"
-                                       style={{display: 'none'}}>
-                                        Import full data of Dropbox
-                                    </a>
-
-
-                                    <div className="embed-responsive embed-responsive-16by9" style={{display: 'none'}}>
-                                        <iframe className="embed-responsive-item"
-                                                src="https://www.dropbox.com/oauth2/authorize?client_id=ycvhx8dgtp4y9ql&response_type=token&redirect_uri=https://c3web.io/beefree/import_dropbox.html">
-                                            Loading ...
-                                        </iframe>
-                                    </div>
-
-
                                 </div>
                             </div>
 
